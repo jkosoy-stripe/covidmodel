b8b56c1f68116ecdcafc6a5f5148c6bae9e7c2e2
Jamie Kosoy <jkosoy@stripe.com>
2025-10-23 19:36:41 -0700

    More Prisma

--- next.config.js
+++ next.config.js
@@ -6,11 +6,6 @@ module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   swcMinify: false, // Use Terser for compatibility
   webpack: (config, {isServer}) => {
-    if (isServer) {
-      // Externalize Prisma to avoid webpack parsing issues
-      config.externals = config.externals || [];
-      config.externals.push('@prisma/client', '.prisma/client');
-    }
     // Uncomment to profile React in production:
     // Object.assign(config.resolve.alias, {
     //   'react-dom$': 'react-dom/profiling',
